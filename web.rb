a449be148d69e39516b1114a6ad9f146e40d2a2d
Joel Gillman <joeltgillman@gmail.com>
2015-02-10 13:02:36 -0800

    Redirect all requests to new domain

--- web.rb
+++ web.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World!"
+helpers do
+  def new_url
+    'https://www.goldbely.com' + request.fullpath
+  end
+end
+
+
+get '/*' do
+  redirect new_url, 301
 end
